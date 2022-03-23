9564db3e75f7e0da8b2358e18840fa7cb132cde4
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-03-23 14:53:22 -0600

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/index.css">
+    <title>IntelEnergy</title>
+</head>
+<body>
+
+
+    <?php include('partials/Banner/banner.html') ?>
+
+    
+    
+</body>
+</html>
