b496a9e257ff974ae2fd6d8e4dad07e3f62ae339
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-03-26 11:32:32 -0600

    add: productos seccion

--- templates/layouts/productos.php
+++ templates/layouts/productos.php
@@ -1,3 +1,9 @@
 
 
-<?php include('templates/components/carrusel.html') ?>
+<section class="productos">
+    <div class="productos__container productos__container--column productos__container--padding">
+        <h2 class="productos__title">PRODUCTOS</h2>
+    </div>
+    
+    <?php include('templates/components/carrusel.html') ?>
+</section>
