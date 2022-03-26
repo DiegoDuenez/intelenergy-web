895cae07690410c9d8bf40e57c594036398495b8
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-03-26 12:46:53 -0600

    add: contacto

--- templates/layouts/contacto.php
+++ templates/layouts/contacto.php
@@ -1,6 +1,18 @@
 
 <section class="contacto">
-    <div class="contacto__container contacto__container--column contacto__container--padding">
+    <div class="contacto__container contacto__container--column contacto__container--padding contacto__container--start">
         <h2 class="contacto__title">CONTACTO</h2>
+    </div>
+    <div class="contacto__container">
+        <div class="contacto__content contacto__content--center">
+            <a class="contacto__text" href="#">BLVD. INDEPENDENCIA #123 COL. CENTRO CP 270000</a>
+            <a class="contacto__text" href="tel:8711234567" >(871) 123 4567</a>
+            <a class="contacto__text" href="tel:8711234567" >(871) 123 4567</a>
+            <a class="contacto__text" href="mail:info@intelenergy.com" >info@intelenergy.com</a>
+
+        </div>
+        <div class="contacto__content">
+
+        </div>
     </div>
 </section>
