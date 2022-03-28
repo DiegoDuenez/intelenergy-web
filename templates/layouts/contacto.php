4a489a333108a18a193a15a40e608c5955503181
DiegoDuenez <19170154@uttcampus.edu.mx>
2022-03-28 10:10:15 -0600

    add: formulario en contacto seccion

--- templates/layouts/contacto.php
+++ templates/layouts/contacto.php
@@ -15,4 +15,25 @@
 
         </div>
     </div>
+    <div class="contacto__container contacto__container--mask contacto__container--center contacto__container--column">
+        <h3 class="contacto__subtitle">PONTE EN CONTACTO CON NOSOTROS</h3>
+        <form class="contacto__form">
+            <div class="contacto__input-group">
+                <input type="text" name="nombre" id="nombre" placeholder="Nombre" class="contacto__input">
+                <input type="text" name="apellidos" id="apellidos" placeholder="Apellidos" class="contacto__input">
+            </div>
+            <div class="contacto__input-group">
+                <input type="email" name="correo" id="correo" placeholder="Correo eléctronico" class="contacto__input">
+                <input type="text" name="numero" id="numero" placeholder="Número celular" class="contacto__input">
+            </div>
+            <div class="contacto__input-group">
+                <input type="text" name="organizacion" id="organizacion" placeholder="Organización" class="contacto__input">
+                <input type="text" name="asunto" id="asunto" placeholder="Asunto" class="contacto__input">
+            </div>
+            <div class="contacto__input-group">
+                <textarea name="mensaje" id="mensaje" placeholder="Mensaje" class="contacto__textarea"></textarea>
+            </div>
+            <input type="submit" value="Enviar" class="contacto__button contacto__button--color">
+        </form>
+    </div>
 </section>
